98c9304066a204caaa14c9ae9d258f2212efdb7d
Amzhm <amziane.hamrani@edu.univ-paris13.fr>
2024-11-05 22:18:26 +0100

    First commit

---
+++ cinetica/app/api/movies/now-playing/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+    const TMDB_API_KEY = process.env.TMDB_API_KEY;
+
+    if (!TMDB_API_KEY) {
+        return NextResponse.json({error: 'TMDB API key is missing'}, {status: 500});
+    }
+
+    try {
+        const response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${TMDB_API_KEY}&language=en-US&page=1`);
+
+        if (!response.ok) {
+            return NextResponse.json({error: 'Failed to fetch data from TMDB'}, {status: response.status});
+        }
+
+        const data = await response.json();
+        return NextResponse.json(data, {status: 200});
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({error: 'Internal Server Error'}, {status: 500});
+    }
+}
