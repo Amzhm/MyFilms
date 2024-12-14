7e8273f0a047f88d855268acc9e4d558d85d4ae7
Amzhm <amziane.hamrani@edu.univ-paris13.fr>
2024-12-14 20:28:21 +0100

    unit tests & e2e tests 3

--- cinetica/jest.config.ts
+++ cinetica/jest.config.ts
@@ -6,7 +6,8 @@ export default {
   testEnvironment: 'jsdom',
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { configFile: './babel.config.js' }]
+    '^.+\\.(js|jsx|ts|tsx)$': "@swc/jest",
   },
+  testMatch:["**/__tests__/All.test.ts"],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
